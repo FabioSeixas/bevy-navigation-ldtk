95ab47099715788079643d228cd895be177f9b6e
fabioseixas <dev.fabioseixas@gmail.com>
2025-12-20 14:49:21 -0300

    rm unused code

--- src/world/grid.rs
+++ src/world/grid.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 
-use bevy_ecs_ldtk::prelude::*;
 use rand::Rng;
 
 use crate::world::components::*;
@@ -19,26 +18,11 @@ impl Grid {
         )
     }
 
-    pub fn world_to_grid(pos: Vec2) -> GridPosition {
-        GridPosition {
-            x: (pos.x / TILE_SIZE) as i32,
-            y: (pos.y / TILE_SIZE) as i32,
-        }
-    }
-
     pub fn get_random_position() -> GridPosition {
         let mut rnd = rand::thread_rng();
         GridPosition {
             x: rnd.gen_range(0..GRID_WIDTH),
             y: rnd.gen_range(0..GRID_HEIGHT),
         }
-    }
-
-    pub fn coords_to_grid_position(c: GridCoords) -> GridPosition {
-        GridPosition { x: c.x, y: c.y }
-    }
-
-    pub fn grid_position_to_coords(gp: GridPosition) -> GridCoords {
-        GridCoords { x: gp.x, y: gp.y }
     }
 }
